20f9db07caa3eace2e6178d126f14381bd6842b5
Subham Kawdal <73130214+Subham-Kawdal-Github@users.noreply.github.com>
2023-01-16 12:43:07 +0530

    Add files via upload

---
+++ springboot-postgresql-crud-example/src/main/java/net/javaguides/springboot/SpringbootPostgresqlCrudExampleApplication.java
@@ -0,0 +1,15 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresqlCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgresqlCrudExampleApplication.class, args);
+	}
+
+}
+
+
